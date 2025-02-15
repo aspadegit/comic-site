045091ccc7d680b85f2c1c14e54052a9a33622ab
Alaine Spade <spadea@ufl.edu>
2025-02-15 02:35:27 -0500

    api calls working

--- src/app/find-comics/find-comics.component.ts
+++ src/app/find-comics/find-comics.component.ts
@@ -3,6 +3,9 @@ import { NgbDropdownModule } from '@ng-bootstrap/ng-bootstrap';
 import {signal} from '@angular/core';
 import { DropdownComponent } from '../dropdown/dropdown.component';
 import { inject } from '@angular/core';
+import { environment } from '../../environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
 
 @Component({
   selector: 'app-find-comics',
@@ -11,10 +14,12 @@ import { inject } from '@angular/core';
   styleUrl: './find-comics.component.css'
 })
 
-
+@Injectable({providedIn: 'root'})
 export class FindComicsComponent implements OnInit {
 
   curDropdownSelection : WritableSignal<number> = signal(0);
+
+  constructor(private http: HttpClient){}
 
   vcr = viewChild('container', {read: ViewContainerRef});
   #filterDropdownRef?: ComponentRef<DropdownComponent>;
@@ -32,6 +37,19 @@ export class FindComicsComponent implements OnInit {
     this.curDropdownSelection.set(index);
   }
 
+  getData() : void
+  {
+    let apiFullUrl:string = "/api/";
+    apiFullUrl = apiFullUrl.concat("search/?api_key=");
+    apiFullUrl = apiFullUrl.concat(environment.apiKey);
+    apiFullUrl = apiFullUrl.concat("&format=json&resources=volume&query=Transformers")
+    this.http.get(`${apiFullUrl}`, {
+   
+    }).subscribe( data => {
+      console.log(data);
+    })
+  }
+
   ngOnInit(): void {
 
       this.#filterDropdownRef = this.vcr()?.createComponent(DropdownComponent);
@@ -39,6 +57,8 @@ export class FindComicsComponent implements OnInit {
 
       this.#sortDropdownRef = this.vcr()?.createComponent(DropdownComponent);
       this.setupDropdownComponent(this.#sortDropdownRef, ['Name', 'Publisher', 'Date'], "Sort Results By");
+      
+      console.log("api url " + environment.apiUrl);
   }
 
 }
